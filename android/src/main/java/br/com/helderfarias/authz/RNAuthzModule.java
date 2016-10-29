98e8c80faf222bd0e21da7bcbff777c532d35d26
Helder Farias <helderfarias@gmail.com>
2016-10-28 21:36:01 -0300

    import initial

--- android/src/main/java/br/com/helderfarias/authz/RNAuthzModule.java
+++ android/src/main/java/br/com/helderfarias/authz/RNAuthzModule.java
@@ -5,45 +5,50 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.provider.Browser;
-import android.support.customtabs.CustomTabsIntent;
 import android.util.Log;
 import android.net.Uri;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Callback;
 import android.app.Activity;
 import android.content.Intent;
 
-
+import android.support.customtabs.CustomTabsIntent;
 import android.content.ComponentName;
+import android.support.customtabs.CustomTabsCallback;
 import android.support.customtabs.CustomTabsClient;
 import android.support.customtabs.CustomTabsIntent;
 import android.support.customtabs.CustomTabsServiceConnection;
 import android.support.customtabs.CustomTabsSession;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
 
-public class RNAuthzModule extends ReactContextBaseJavaModule  {
+import org.json.JSONObject;
+import org.json.JSONException;
 
-    private static final String CUSTOM_TAB_PACKAGE_NAME = "com.android.chrome";
+public class RNAuthzModule extends ReactContextBaseJavaModule implements ActivityEventListener {
+
     private static final String TAG = "RNAuthzModule";
 
     private CustomTabsClient mCustomTabsClient;
     private CustomTabsSession mCustomTabsSession;
     private CustomTabsServiceConnection mCustomTabsServiceConnection;
-    private CustomTabsIntent mCustomTabsIntent;
+    private CustomTabsCallback mSession;
+    private EventManager eventManager;
 
     public RNAuthzModule(ReactApplicationContext reactContext) {
         super(reactContext);
+        this.eventManager = new EventManager(reactContext);
+        reactContext.addActivityEventListener(this);
     }
 
     @Override
     public String getName() {
         return "RNAuthz";
-    }
-
-    boolean httpOrHttpsScheme(String url) {
-        return url.startsWith("http") || url.startsWith("https");
     }
 
     @ReactMethod
@@ -77,35 +82,63 @@ public class RNAuthzModule extends ReactContextBaseJavaModule  {
 
     @ReactMethod
     public void dismiss() {
+        this.eventManager.send("InAppBrowserTabOnDismiss");
     }
 
     @ReactMethod
     public void isAvailable(Callback error) {
-        this.bootstrap();
+        boolean ok = this.bootstrap();
+
+        if (!ok) {
+            error.invoke(true);
+        } else {
+            error.invoke(false);
+        }
     }
 
-    private void bootstrap() {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Ignore
+    }
+
+    public void onNewIntent(Intent intent) {
+        WritableMap params = Arguments.createMap();
+
+        params.putString("url", intent.getDataString());
+
+        this.eventManager.send("InAppBrowserTabOnDismiss", params);
+    }
+
+    private boolean httpOrHttpsScheme(String url) {
+        return url.startsWith("http") || url.startsWith("https");
+    }
+
+    private boolean bootstrap() {
         if (mCustomTabsServiceConnection != null) {
-            return;
+            return true;
         }
 
-        final Activity activity = getCurrentActivity();
+        String packageName = CustomTabsHelper.getPackageNameToUse(getReactApplicationContext());
+        if (packageName == null || packageName.isEmpty()) {
+            return false;
+        }
 
         mCustomTabsServiceConnection = new CustomTabsServiceConnection() {
             @Override
-            public void onCustomTabsServiceConnected(ComponentName componentName, CustomTabsClient customTabsClient) {
-                mCustomTabsClient= customTabsClient;
+            public void onCustomTabsServiceConnected(ComponentName componentName, CustomTabsClient client) {
+                mCustomTabsClient = client;
                 mCustomTabsClient.warmup(0L);
                 mCustomTabsSession = mCustomTabsClient.newSession(null);
+                eventManager.send("InAppBrowserTabOnShow");
             }
 
             @Override
             public void onServiceDisconnected(ComponentName name) {
-                mCustomTabsClient= null;
+                eventManager.send("InAppBrowserTabOnDismiss");
+                mCustomTabsClient = null;
             }
         };
 
-        CustomTabsClient.bindCustomTabsService(activity, CUSTOM_TAB_PACKAGE_NAME, mCustomTabsServiceConnection);
+        return CustomTabsClient.bindCustomTabsService(getCurrentActivity(), packageName, mCustomTabsServiceConnection);
     }
 
 }
